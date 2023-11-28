b00f113d5995409cf930f4c73b804e44559e2063
Elina Myagkova <kisterlina@gmail.com>
2023-11-28 11:19:43 +0300

    i love testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,18 @@
 import pytest
 from selene import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def browser_management():
-    browser.config.base_url = 'https://app.qa.guru/automation-practice-form/'
-    browser.config.timeout = 4.0
-    driver_options = webdriver.ChromeOptions()
-    driver_options.add_argument('--headless=new')
+    browser.config.base_url = "https://demoqa.com"
     browser.config.window_width = 1024
     browser.config.window_height = 1366
+    browser.config.timeout = 4
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = "eager"
+    browser.config.driver_options = driver_options
 
     yield
 
--- tests/test.py
+++ tests/test.py
@@ -1,17 +1,59 @@
-from selene import browser, by, be
+from selene import browser, by, be, have
+import os.path
+
+import tests
 
 
-def test_qa_registration():
-    browser.config.base_url = 'https://app.qa.guru/automation-practice-form/'
-    browser.open('/')
-
-    browser.element('.MuiBox-root .css-1vpe9z').should(be.visible).click().element('MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv')
-    browser.element('#:r0:').type('Elina').press_enter()
-    browser.element('#:r1:').type('Myagkova').press_enter()
-    browser.element('#:r2:').type('kisterlina@gmail.com').press_enter()
-    browser.element('#:r3:').type('+79996662211').press_enter()
-    browser.element('MuiInputBase-input').click().element(
-        by.text('Russian')).click()
-    browser.element(':r4:').type('07071990').press_enter()
+def should():
+    pass
 
 
+def test_qa_registration(browser_management):
+    browser.open("/automation-practice-form")
+
+    # WHEN
+    browser.element("#firstName").should(be.blank).type("Elina")
+    browser.element("#lastName").should(be.blank).type("Myagkova")
+    browser.element("#userEmail").type("kisterlina@gmail.com")
+    browser.element("#gender-radio-2").double_click()
+    browser.element("#userNumber").type(9009998877)
+
+    browser.element("#dateOfBirthInput").click()
+    browser.element(".react-datepicker__month-select").click().element(
+        by.text("July")
+    ).click()
+    browser.element(".react-datepicker__year-select").click().element(
+        by.text("1990")
+    ).click()
+    browser.element(".react-datepicker__day--007").click()
+
+    browser.element("#subjectsInput").set_value("History").press_enter()
+    browser.element('[for="hobbies-checkbox-2"]').click()
+
+    browser.element("#uploadPicture").set_value(
+        os.path.abspath(
+            os.path.join(os.path.dirname("tests"), "picture/Test vertical 640px.jpg")
+        )
+    )
+
+    # THEN
+    browser.element("#currentAddress").should(be.blank).type("India")
+    browser.element("#react-select-3-input").type("Haryana").press_enter()
+    browser.element("#react-select-4-input").type("Panipat").press_enter()
+    browser.element("#submit").press_enter()
+
+    browser.all("tbody tr:last-child").should(
+        have.exact_texts(
+            "Elina Myagkova",
+            "kisterlina@gmail.com",
+            "Female",
+            "9009998877",
+            "07 July,1990",
+            "History",
+            "Reading",
+            # "Test vertical 640px.jpg",
+            "India",
+            "Haryana Panipat",
+        )
+    )
+    browser.element("#closeLargeModal").press_enter()
